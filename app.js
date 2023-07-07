8090f6f1dd58c241920b336d7a66a94cf59795d9
sumit <mazumdarsumit3@gmail.com>
2023-07-07 14:32:41 +0530

    cors enabled

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const app = express();
 const authRouter = require("./router/authRouter.js");
 const dbConnect = require("./config/databaseConfig.js");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 dbConnect();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URl],
+    credentials: true,
+  })
+);
 
 app.use("/api/auth/", authRouter);
 
